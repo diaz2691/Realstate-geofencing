490d8e7ed1d1ae08d9e1d1880d1013b1b5d820bc
Jose Diaz <jodiaz@csumb.edu>
2016-12-28 01:01:39 -0800

    Testing autodeploy

--- deploy.php
+++ deploy.php
@@ -30,7 +30,7 @@
   <meta charset="UTF-8">
   <title>GIT DEPLOYMENT SCRIPT</title>
 </head>
-<body style="background-color: #000000; color: #007F00; font-weight: bold; padding: 0 10px;">
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <pre>
  .  ____  .    ____________________________
  |/      \|   |                            |
