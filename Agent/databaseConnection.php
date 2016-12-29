ab7e1e680bfdb0c0f6ce439a6eb2fd5e6b4af6f0
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2016-12-28 17:51:57 -0800

    Testing inserting functionality

--- Agent/databaseConnection.php
+++ Agent/databaseConnection.php
@@ -1,4 +1,4 @@
-<?php
+<!--<?php
 function getConnection() {
     //database login credentials
 	$host = "localhost";
@@ -32,3 +32,15 @@ function sendQuery($sql, $namedParameters){
         }
 }
 ?>
+
+-->
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <h1>Your information has been added.</h1>
+
+        <p>Thank you!</p>
+
+    </body>
+</html>
