4e494588b81a6d6f8ac4fe46acf37e0bc18eb8d3
diaz2691 <jodiaz@csumb.edu>
2016-12-29 23:09:32 -0800

    Checking code to make pdf

--- Admin/commisionSheet.php
+++ Admin/commisionSheet.php
@@ -5,7 +5,7 @@ require('fpdf/fpdf.php');
 
 
 
-$the_content = "Ut sagittis erat vitae nunc viverra, ut bibendum dui sodales. In fermentum, augue vel vestibulum porttitor, lectus ipsum faucibus justo, tincidunt luctus velit odio quis orci. ";
+$the_content = "Ut sagittis erat vitae nunc viverra, ut bibendum dui sodales./n In fermentum, augue vel vestibulum porttitor, lectus ipsum faucibus justo, tincidunt luctus velit odio quis orci. ";
 
 $pdf = new FPDF();
 $pdf->AddPage();
