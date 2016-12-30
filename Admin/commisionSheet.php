328e1344c0f7cce80d65dfcf40ea25720d64caf8
diaz2691 <jodiaz@csumb.edu>
2016-12-29 22:18:57 -0800

    Adding files for creating pdf file

--- Admin/commisionSheet.php
+++ Admin/commisionSheet.php
@@ -37,5 +37,5 @@ $pdf->AddPage();
 $pdf->SetFont('Times','',12);
 for($i=1;$i<=40;$i++)
     $pdf->Cell(0,10,'Printing line number '.$i,0,1);
-$pdf->Output();
+$pdf->Output('Commision Sheet.pdf','I');
 ?>
