8218649479f63fce94f9943394d9c8c638f51f80
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2016-12-30 13:16:30 -0800

    Editing agent info debugged

---
+++ Admin/editAgentInfo.php
@@ -0,0 +1,94 @@
+
+<?php
+    require("../databaseConnection.php");  
+    session_start();
+    $dbConn = getConnection();
+
+    if(!isset($_SESSION['userId'])) {
+	    header("Location: userLogin.php?error=wrong username or password");
+    }
+
+    if (isset($_GET['userId'])){
+
+         $userId = $_GET['userId'];
+        
+         $sql = "SELECT * FROM UsersInfo WHERE userId = '".$userId."'"; // need to get assignmentId instead of the one for testing purposes
+         $stmt = $dbConn -> prepare($sql);
+         $stmt->execute();
+         $results = $stmt->fetchAll();
+         foreach($results as $result) { 
+                $username = htmlspecialchars($result['username']);
+                $firstName = htmlspecialchars($result['firstName']);
+                $email = htmlspecialchars($result['email']);
+                $phone = htmlspecialchars($result['phone']);
+                $lastName = htmlspecialchars($result['lastName']);
+                $license = htmlspecialchars($result['license']);
+         }
+
+    }
+
+     if (isset($_POST['editForm'])) {  //the update form has been submitted
+         $sql = "UPDATE UsersInfo
+                 SET firstName = :firstName,
+                        lastName = :lastName,
+                        email = :email,
+                        phone = :phone,
+                        username = :username,
+                        license = :license
+                 WHERE userId = :userId";
+          $namedParameters = array();
+          $namedParameters[":firstName"] = $_POST['firstName'];
+          $namedParameters[":lastName"] = $_POST['lastName'];
+          $namedParameters[":email"] = $_POST['email'];     
+          $namedParameters[":phone"] = $_POST['phone'];     
+          $namedParameters[":username"] = $_POST['username'];     
+          $namedParameters[":license"] = $_POST['license'];  
+          $namedParameters[":userId"] = $_POST['userId'];;      
+          $stmt = $dbConn -> prepare($sql);
+          $stmt->execute($namedParameters);
+          echo "Record has been updated!";
+             header("Location: viewAgents.php");
+         }
+
+
+
+    ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Update Visitor</title>
+    <meta charset = "utf-8"/>
+    <link rel="stylesheet" type="text/css" href="css/navStyles.css">
+    <link type="text/css" rel="stylesheet" href="css/mainHeaderStyles.css">
+    <link type="text/css" rel="stylesheet" href="css/updateAssignmentStyles.css">
+    <link type="text/css" rel="stylesheet" href="css/backgroundStyles.css">
+    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script><!-- importing jQuery library-->
+
+</head>
+    <body>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    <h1 id="mainHeader">Visitors Information</h1>    
+    <h1>Edit Record</h1>
+        <div id="formDiv">
+            
+      <br/> 
+    <form method="post">
+
+        Username: <input type="text" name="username" value="<?=$username?>"> <br />
+        First Name: <input type="text" name="firstName" value="<?=$firstName?>"><br />
+        Last Name: <input type="text" name="lastName" value="<?=$lastName?>"><br />
+        Email: <input type="text" name="email" value="<?=$email?>"><br />
+        Phone: <input type="text" name="phone" value="<?=$phone?>"><br />
+        License: <input type="text" name="license" value="<?=$license?>"><br />
+        <input type="hidden" name="userId" value="<?=$userId?>"> 
+<br/>
+        <input id="button" type="submit" name="editForm" value="Edit!">
+    
+    </form>
+            
+        </div>         
+        
+    </body>
+</html>
