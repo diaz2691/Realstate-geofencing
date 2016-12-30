26fa1eb086fc3caa9e07011f6eb39b323c80670c
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2016-12-29 22:01:45 -0800

    Agent is now able to add new houses

---
+++ Agent/addHouse.php
@@ -0,0 +1,79 @@
+<?php
+    require("../databaseConnection.php");  
+    session_start();
+    $dbConn = getConnection();
+
+    if(!isset($_SESSION['userId'])) {
+        header("Location: userLogin.php?error=wrong username or password");
+    } 
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add New House Information</title>
+    <meta charset = "utf-8"/>
+    <link rel="stylesheet" type="text/css" href="css/navStyles.css">
+    <link type="text/css" rel="stylesheet" href="css/mainHeaderStyles.css">
+    <link type="text/css" rel="stylesheet" href="css/enterAssignmentsStyles.css">
+    <link type="text/css" rel="stylesheet" href="css/backgroundStyles.css">
+    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script><!-- importing jQuery library-->
+
+</head>
+    <body>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><h1 id="mainHeader">Add New House Information</h1><br/>
+        
+        <!-- Navigation Bar -->
+        <?php
+            //require("teacherNav.php");
+            //generateTeacherNav();
+        ?>
+        <!---------------------->
+        
+        <br/><br/><br/>
+        <h1 id="enterHouseHeader">Enter House Information</h1>
+        <div id="enterHouseDiv" > <br/>
+            <div id="innerDiv">
+
+                Address: <input type="text" name="address"> <br />
+                City: <input type="text" name="city"><br />
+                State: <input type="text" name="state"><br />
+                Zip: <input type="text" name="zip"><br />
+                Bedrooms: <input type="text" name="bedrooms"><br />
+                Bathrooms: <input type="text" name="bathrooms"><br />
+                Price: <input type="text" name="price"><br />
+                <input type="hidden" name="userId" value="<?=$_SESSION['userId']?>"> 
+                <input id="enterHouse" type="button" value="Enter" >  
+                
+            </div>
+        </div>
+        
+        <script>
+
+            $("#enterHouse").click( function(event){
+                var address = $("#address").val();
+                var city = $("#city").val();
+                var state = $("#state").val();
+                var zip = $("#zip").val();
+                var bedrooms = $("#bedrooms").val();
+                var bathrooms = $("#bathrooms").val();
+                var price = $("#price").val();
+                var userId = $("#userId").val();               
+                
+                $.ajax({
+                    type: "POST",
+                    url: "submitHomeInfo.php",
+                    data: {studentid: studentid,
+                          title: title,
+                          grade: grade,
+                          possiblepoints: possiblepoints,
+                          feedback: feedback}
+                }); 
+                window.location.href = "viewGrades.php";
+            });      
+        </script>
+        
+    </body>
+</html>
---
+++ Agent/sumbitHomeInfo.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    require('../databaseConnection.php');
+    $dbConn = getConnection();
+
+    $sql = "INSERT INTO HouseInfo
+                 (userId, address, city, state, zip, bedrooms, bathrooms, price)
+                 VALUES (userId, address, city, state, zip, bedrooms, bathrooms, price)";
+          $namedParameters = array();
+          $namedParameters[":userId"] = $_POST['userId'];
+          $namedParameters[":address"] = $_POST['address'];
+          $namedParameters[":city"] = $_POST['city'];
+          $namedParameters[":state"] = $_POST['state'];     
+          $namedParameters[":zip"] = $_POST['zip'];     
+          $namedParameters[":bedrooms"] = $_POST['bedrooms'];     
+          $namedParameters[":bathrooms"] = $_POST['bathrooms'];     
+          $namedParameters[":price"] = $_POST['price'];     
+          $stmt = $dbConn -> prepare($sql);
+          $stmt->execute($namedParameters);
+          header("Location: AgentProfile.php");
+?>
