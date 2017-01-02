c7246ee4d820b65ef2f61f21b98b62f5186e2a0d
diaz2691 <jodiaz@csumb.edu>
2017-01-02 14:19:27 -0800

    checking that new agent gets added

--- Admin/submitAgentInfo.php
+++ Admin/submitAgentInfo.php
@@ -5,19 +5,6 @@
     use Twilio\Rest\Client;
     $dbConn = getConnection();
 
-    $account_sid = "AC4991f00911beb00578efd8b8355fdc7d";
-    $auth_token = "b605b8121c246b4b64fe407255f50528";
-    $twilio_phone_number = "+18315851661";
-
-    $client = new Client($account_sid, $auth_token);
-    $client->account->messages->create(
-    $_POST['phone'],
-    array(
-    "From" => $twilio_phone_number,
-    "Body" => "Your password is " . $_POST['password'],
-    )
-    );
-
     $sql = "INSERT INTO UsersInfo
                  (userType, username, password, firstName, lastName, email, phone, license)
                  VALUES (1, :username, :password, :firstName, :lastName, :email, :phone, :license)";
@@ -31,6 +18,20 @@
           $namedParameters[":license"] = $_POST['license'];   
           $stmt = $dbConn -> prepare($sql);
           $stmt->execute($namedParameters);
+    $account_sid = "AC4991f00911beb00578efd8b8355fdc7d";
+    $auth_token = "b605b8121c246b4b64fe407255f50528";
+    $twilio_phone_number = "+18315851661";
+
+    $client = new Client($account_sid, $auth_token);
+    $client->account->messages->create(
+    $_POST['phone'],
+    array(
+    "From" => $twilio_phone_number,
+    "Body" => "Your password is " . $_POST['password'],
+    )
+    );
+
+    
 
   
 
