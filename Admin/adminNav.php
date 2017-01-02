093a35a3958bdde01c6fef5e73dbb0ddfde2476f
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-01-02 00:42:58 -0800

    Navigation bar added to admin

--- Admin/adminNav.php
+++ Admin/adminNav.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="adminNav.css">
         <script src="adminNav.js"></script>
     </head>
