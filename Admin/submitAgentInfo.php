9fb360c7a537ab4bb68a1b181fa9cbc8d3740c07
diaz2691 <jodiaz@csumb.edu>
2017-01-02 13:18:07 -0800

    Fixing sending sms with twilio when adding agent

--- Admin/submitAgentInfo.php
+++ Admin/submitAgentInfo.php
@@ -2,14 +2,14 @@
     session_start();
     require('../databaseConnection.php');
     require_once '../twilio-php-master/Twilio/autoload.php';
-
+    use Twilio\Rest\Client;
     $dbConn = getConnection();
 
     $account_sid = "AC4991f00911beb00578efd8b8355fdc7d";
     $auth_token = "b605b8121c246b4b64fe407255f50528";
     $twilio_phone_number = "+18315851661";
 
-    $client = new Services_Twilio($account_sid, $auth_token);
+    $client = new Client($account_sid, $auth_token);
     $client->account->messages->create(array(
     "From" => $twilio_phone_number,
     "To" => "phone",
