c0b469fc8fba06c40465c389440aec157b5bd47f
Jose Diaz <jodiaz@csumb.edu>
2017-01-09 01:11:38 -0800

    Adding view commission page

--- Admin/adminNav.php
+++ Admin/adminNav.php
@@ -36,7 +36,7 @@
                                 <li class=" dropdown">
                                     <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">View Forms</a>
                                 </li>
-                                <li><a href="commisionSheet.php">Open PDF</a></li>
+                                <li><a href="viewCommissionSheet.php">View Commission Sheets</a></li>
                                 <li><a href="#">Add New</a></li>
                             </ul>
                         </li>
