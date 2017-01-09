987eae6a973186700773cd691becade89888283f
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-01-08 21:42:56 -0600

    Added agent column next to house in admin profile

--- Admin/viewHouses.php
+++ Admin/viewHouses.php
@@ -93,13 +93,13 @@ To change this template use Tools | Templates.
 
             $dbConn = getConnection();
 
-            $sql = "SELECT date(dateTimes), address, city, state, zip, bedrooms,
+            $sql = "SELECT date(dateTimes) as dateTimes, address, city, state, zip, bedrooms,
                         bathrooms, price, firstName, lastName 
                     FROM HouseInfo, UsersInfo
                     where HouseInfo.userId = UsersInfo.userId
                     ORDER BY dateTimes ASC";
             if($sortByDate == false){
-                $sql = "SELECT date(dateTimes), address, city, state, zip, bedrooms,
+                $sql = "SELECT date(dateTimes) as dataTimes, address, city, state, zip, bedrooms,
                             bathrooms, price, firstName, lastName
                         FROM HouseInfo, UsersInfo
                         WHERE  HouseInfo.userId = UsersInfo.userId
