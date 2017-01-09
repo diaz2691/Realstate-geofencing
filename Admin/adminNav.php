e5f505530681d7df85f73f62725afa71637f930a
Jose Diaz <jodiaz@csumb.edu>
2017-01-09 11:51:46 -0800

    Trying to fix viewing commission sheets

--- Admin/adminNav.php
+++ Admin/adminNav.php
@@ -34,9 +34,11 @@
                             <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Commission Form<span class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <li class=" dropdown">
-                                    <a href="viewCommissionSheet.php" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">View Commission Sheets</a>
+                                    <a href="viewCommissionSheet.php" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">View Commission Sheets<span class="caret"></span></a>
                                 </li>
-                                <li><a href="#">Add New</a></li>
+                                <li>
+                                    <a href="#">Add New</a>
+                                </li>
                             </ul>
                         </li>
                         <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Agents <span class="caret"></span></a>
