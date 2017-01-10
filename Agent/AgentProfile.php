095e67ce164fbf97faacc7e054d694555d99bccd
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-01-09 19:56:59 -0600

    Adding agent profile plus nav

--- Agent/AgentProfile.php
+++ Agent/AgentProfile.php
@@ -18,28 +18,31 @@
   </head>
 
   <body>
-
+        <!-- Navigation Bar -->
+        <?php
+            require("adminNav.php");
+        ?>
     
-<figure class="snip0057 red">
-  <figcaption>
-    <h2>Brayanne <span>Reyes Ron</span></h2>
-    <p>I am an agent with much experience in the field of selling homes. It is my passion to work with buyers and ultimately selling them their dream home.</p>
-    <div class="icons"><a href="#"><i class="ion-ios-home"></i></a><a href="#"><i class="ion-ios-email"></i></a><a href="#"><i class="ion-ios-telephone"></i></a></div>
-  </figcaption>
-  <div class="image"><img src="http://mvptitle.com/blog/wp-content/uploads/2014/01/home_seller.jpg" alt="sample4"/></div>
-  <div class="position">Agent</div>
-</figure>
-<!--
-<figure class="snip0057 blue">
-  <figcaption>
-    <h2>Tiffany <span>Case</span></h2>
-    <p>That's the whole problem with science. You've got a bunch of empiricists trying to describe things of unimaginable wonder.</p>
-    <div class="icons"><a href="#"><i class="ion-ios-home"></i></a><a href="#"><i class="ion-ios-email"></i></a><a href="#"><i class="ion-ios-telephone"></i></a></div>
-  </figcaption>
-  <div class="image"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample3.jpg" alt="sample3"/></div>
-  <div class="position">Software Engineer</div>
-</figure>
--->
+        <figure class="snip0057 red">
+          <figcaption>
+            <h2>Brayanne <span>Reyes Ron</span></h2>
+            <p>I am an agent with much experience in the field of selling homes. It is my passion to work with buyers and ultimately selling them their dream home.</p>
+            <div class="icons"><a href="#"><i class="ion-ios-home"></i></a><a href="#"><i class="ion-ios-email"></i></a><a href="#"><i class="ion-ios-telephone"></i></a></div>
+          </figcaption>
+          <div class="image"><img src="http://mvptitle.com/blog/wp-content/uploads/2014/01/home_seller.jpg" alt="sample4"/></div>
+          <div class="position">Agent</div>
+        </figure>
+        <!--
+        <figure class="snip0057 blue">
+          <figcaption>
+            <h2>Tiffany <span>Case</span></h2>
+            <p>That's the whole problem with science. You've got a bunch of empiricists trying to describe things of unimaginable wonder.</p>
+            <div class="icons"><a href="#"><i class="ion-ios-home"></i></a><a href="#"><i class="ion-ios-email"></i></a><a href="#"><i class="ion-ios-telephone"></i></a></div>
+          </figcaption>
+          <div class="image"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample3.jpg" alt="sample3"/></div>
+          <div class="position">Software Engineer</div>
+        </figure>
+        -->
     
     
     
