391e95f19bfd3cfdd4eed0bcc8760a0c6e4e123b
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-01-16 21:00:28 -0600

    Added customers page on admin profile

---
+++ Admin/viewCustomers.php
@@ -0,0 +1,116 @@
+<?php
+    require("../databaseConnection.php");  
+    session_start();
+    $dbConn = getConnection();
+
+    if(!isset($_SESSION['userId'])) {
+	    header("Location: ../index.html?error=wrong username or password");
+    } 
+
+    if (isset ($_GET['deleteForm'])){  //checking whether we have clicked on the "Delete" button
+        $sql = "DELETE FROM BuyerInfo 
+                 WHERE buyerID = '".$_GET['buyerID']."'";
+        $stmt = $dbConn -> prepare($sql);
+        $stmt->execute();
+
+    }
+ ?>
+
+        
+        <!--
+To change this template use Tools | Templates.
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    
+<head>
+    <title>Visitors</title>
+    
+    <script>
+        
+            function confirmDelete(record) {
+               // alert("hi"); // for testing
+               var deleteRecord = confirm("Are you sure you want to delete " + record + "?");
+               if(!deleteRecord){
+                   return false
+               } else {
+                   return true;
+               }
+            }
+        
+        </script>
+    
+    <meta charset = "utf-8"/>
+ <style type="text/css">
+               /* .tableHeader {
+                    text-align:center;
+                }*/
+
+              .tableButtons {
+                text-align:center;
+              }
+              .option {
+              font-family: "Roboto", sans-serif;
+              outline: 0;
+              background: "green";
+              border: 0;
+              box-sizing: border-box;
+              font-size: 14px;
+              text-align:center;
+              background-color:#c68c53
+            }
+            .tftable {font-size:14px;color:#fbfbfb;width:100%;border-width: 1px;border-color: #686767;border-collapse: collapse;}
+            .tftable th {font-size:14px;background-color:#c68c53;border-width: 1px;padding: 8px;border-style: solid;border-color: #686767;text-align:left;}
+            .tftable tr {background-color:#d2a679;}
+            .tftable td {font-size:14px;border-width: 1px;padding: 8px;border-style: solid;border-color: #686767;}
+            .tftable tr:hover {background-color:#c68c53;}
+    </style>
+</head>
+    
+
+    <body>
+                        <!-- Navigation Bar -->
+        <?php
+            require("adminNav.php");
+        ?> 
+        <br/><br/><h2 id="header2">Customers &#x2713</h2> 
+        
+        <table class="tftable" border="1">
+       
+        <tr><th>Agent Name</th><th>Customer Name</th><th>Email</th><th>Phone</th><th>Bedrooms</th><th>Bathrooms</th><th>Price</th></tr>    
+            
+            <?php
+
+            function getAgentName($id){
+                $dbConn = getConnection();
+                $sql = "SELECT firstName, lastName FROM UsersInfo where userId = $id";
+                $stmt = $dbConn -> prepare($sql);
+                $stmt->execute();
+                //$stmt->execute();
+                $results = $stmt->fetchAll();
+                return $results['firstName'] . ' ' . $results['lastName'];
+            }
+
+            $dbConn = getConnection();
+            $sql = "SELECT * FROM BuyerInfo";
+            $stmt = $dbConn -> prepare($sql);
+            $stmt->execute();
+            //$stmt->execute();
+            $results = $stmt->fetchAll();
+
+            foreach($results as $result){
+                echo "<tr>";
+                echo "<td>" . getAgentName($result['userId']) . "</td>";
+                echo "<td>" . $result['firstName'] . ' ' . $result['lastName'] . "</td>";
+                echo "<td>" . htmlspecialchars($result['email']) . "</td>";
+                echo "<td>" . htmlspecialchars($result['phone']) . "</td>";
+                echo "<td>" . htmlspecialchars($result['bedrooms']) . "</td>";
+                echo "<td>" . htmlspecialchars($result['bathrooms']) . "</td>";
+                echo "<td>" . htmlspecialchars($result['price']) . "</td>";
+                echo "</tr>";   
+               } //closes foreach
+             ?>         
+        </table>
+    <br/><br/><br/><br/><br/><br/>
+    </body>
+</html>
