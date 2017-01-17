9bd23400b028844d1a55b5dcff74920c0e684ac0
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-01-16 21:51:51 -0600

    Added change password page

--- changePassword.php
+++ changePassword.php
@@ -1,31 +1,5 @@
-<!--<?php
-    require("databaseConnection.php");  
-    session_start();
-    $dbConn = getConnection();
 
-    if(!isset($_SESSION['userId'])) {
-        header("Location: ../index.html?error=wrong username or password");
-    } 
-
-    if(isset($_POST['oldPassword'])) {
-        $sql = "SELECT password FROM UsersInfo where userId = $_SESSION['userId']";
-        $stmt = $dbConn -> prepare($sql);
-        $stmt->execute();
-        //$stmt->execute();
-        $results = $stmt->fetch();
-        if($results['password'] == $_POST['oldPassword'] ){
-            $sql = "UPDATE BuyerInfo
-                 SET password = :password
-                 WHERE userId = :userId";
-            $namedParameters = array();
-            $namedParameters[":password"] = $_POST['oldPassword'];
-            $namedParameters[":userId"] = $_SESSION['userId'];     
-            $stmt = $dbConn -> prepare($sql);
-            $stmt->execute($namedParameters);
-        }
-        header("Location: index.html");
-    }
- ?>
+<!--
 To change this template use Tools | Templates.
 -->
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -39,16 +13,31 @@ To change this template use Tools | Templates.
             font-family: "Roboto", sans-serif;
         }
     </style>
-    <title>Change Password</title>
+    <title></title>
 </head>
 <body>
     <div class="login-page">
-        <h1> Change Password </h1>
+        <h1> Login Screen </h1>
         <div class="form">
-            <form action="" method="post" class="login-form">
-                <input type="text" name="oldPassword" placeholder="old password"/>
-                <input type="password" name="newPassword" placeholder="new password"/>
+            <form action="login.php" method="post" class="login-form">
+                <input type="text" name="username" placeholder="username"/>
+                <input type="password" name="password" placeholder="password"/>
                 <input type="submit" value="login" name="loginForm" id="button"/>      
             </form>
+    
+    <h3 style="color:red">
+    <?php
+  
+      if (isset($_GET['error'])) {
+          
+          echo $_GET['error'];
+          
+      }
+
+    ?>
+    </h3>
+
+
+
 </body>
 </html>
