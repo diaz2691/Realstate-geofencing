1fef3829b236c7d5150f0609dc359affc6e3e1f8
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-01-20 17:20:26 -0600

    Adding self filling functionality when inputing zip code to fill in city and state

--- Agent/addHouse.php
+++ Agent/addHouse.php
@@ -25,7 +25,7 @@
                 dataType: "json",
                 data: {"address": $("#zip").val() },
                 success: function(data,status) {
-                     $("#cities").html(data);
+                     $("#city").html(data);
                 },
                 complete: function(data,status) { //optional, used for debugging purposes
                      //alert(status);
