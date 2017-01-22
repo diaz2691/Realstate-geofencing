ef721f6f685f3a886bb403d25ed47eb3f7081b4c
Jose Diaz <jodiaz@csumb.edu>
2017-01-21 16:02:47 -0800

    prototype of commission broker fee calculations - testing needed

--- Admin/submitCommInfo.php
+++ Admin/submitCommInfo.php
@@ -41,101 +41,101 @@
 
 
 
-    if($TYGross <= 80000)
-    {
-      $difference =  80000 - $TYGross;
+    // if($TYGross <= 80000)
+    // {
+    //   $difference =  80000 - $TYGross;
 
-      //$total = $TYGross + $commission;
-      if($commission <= $difference)
-      {
-        $brokerFee += $commission * .20;
-      }
-      else
-      {
-        $brokerFee += $difference * .20;
-        $commission = $commission - $difference;
-        if($commission > 0)
-        {
-          $difference = 49999 - $commission;
-          if($difference <= 49999)
-          {
-            $brokerFee += $difference * .15;
-          }
-          else
-          {
-            $brokerFee += 49999 * .15;
-            $commission = $commission - 49999;
-            if($commission > 0)
-            {
-              $difference = 49999 - $commission;
-              if($difference <= 49999)
-              {
-                $brokerFee += $difference * .10;
-              }
-              else
-              {
-                $brokerFee += 49999 * .10;
-                $commission = $commission - 49999;
-                if($commission > 0)
-                {
-                  $brokerFee += $commission * .5;
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    else if ($TYGross <= 130000)
-    {
-      $difference =  130000 - $TYGross; 
-      if($commission <= $difference)
-      {
-        $brokerFee += $commission * .15;
-      }
-      else
-      {
-        $brokerFee += $difference * .15;
-        $commission = $commission - $difference;
-        if($commission > 0)
-        {
-          if($difference <= 49999)
-          {
-            $brokerFee += $difference * .10;
-          }
-          else
-          {
-            $brokerFee += 49999 * .10;
-            $commission = $commission - 49999;
-            if($commission > 0)
-            {
-              $brokerFee += $commission * .5;
-            }
-          }
-        }
-      }
-    }
-    else if($TYGross <= 180000)
-    {
-      $difference =  180000 - $TYGross; 
-      if($commission <= $difference)
-      {
-        $brokerFee += $commission * .15;
-      }
-      else
-      {
-        $brokerFee += 49999 * .10;
-        $commission = $commission - 49999;
-        if($commission > 0)
-        {
-          $brokerFee += $commission * .5;
-        }
-      }
-    }
-    else
-    {
-      $brokerFee += $commission * .5;
-    }
+    //   //$total = $TYGross + $commission;
+    //   if($commission <= $difference)
+    //   {
+    //     $brokerFee += $commission * .20;
+    //   }
+    //   else
+    //   {
+    //     $brokerFee += $difference * .20;
+    //     $commission = $commission - $difference;
+    //     if($commission > 0)
+    //     {
+    //       $difference = 49999 - $commission;
+    //       if($difference <= 49999)
+    //       {
+    //         $brokerFee += $difference * .15;
+    //       }
+    //       else
+    //       {
+    //         $brokerFee += 49999 * .15;
+    //         $commission = $commission - 49999;
+    //         if($commission > 0)
+    //         {
+    //           $difference = 49999 - $commission;
+    //           if($difference <= 49999)
+    //           {
+    //             $brokerFee += $difference * .10;
+    //           }
+    //           else
+    //           {
+    //             $brokerFee += 49999 * .10;
+    //             $commission = $commission - 49999;
+    //             if($commission > 0)
+    //             {
+    //               $brokerFee += $commission * .5;
+    //             }
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+    // else if ($TYGross <= 130000)
+    // {
+    //   $difference =  130000 - $TYGross; 
+    //   if($commission <= $difference)
+    //   {
+    //     $brokerFee += $commission * .15;
+    //   }
+    //   else
+    //   {
+    //     $brokerFee += $difference * .15;
+    //     $commission = $commission - $difference;
+    //     if($commission > 0)
+    //     {
+    //       if($difference <= 49999)
+    //       {
+    //         $brokerFee += $difference * .10;
+    //       }
+    //       else
+    //       {
+    //         $brokerFee += 49999 * .10;
+    //         $commission = $commission - 49999;
+    //         if($commission > 0)
+    //         {
+    //           $brokerFee += $commission * .5;
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+    // else if($TYGross <= 180000)
+    // {
+    //   $difference =  180000 - $TYGross; 
+    //   if($commission <= $difference)
+    //   {
+    //     $brokerFee += $commission * .15;
+    //   }
+    //   else
+    //   {
+    //     $brokerFee += 49999 * .10;
+    //     $commission = $commission - 49999;
+    //     if($commission > 0)
+    //     {
+    //       $brokerFee += $commission * .5;
+    //     }
+    //   }
+    // }
+    // else
+    // {
+    //   $brokerFee += $commission * .5;
+    // }
 
     $message = "wrong answer";
     echo "<script type='text/javascript'>alert('$userResults');</script>";
