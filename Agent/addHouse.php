d2b3461cce678054fbe0d118b6a243c6b8e05024
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-01-21 21:43:28 -0600

    Debuggin option that does not display city and state after inputin zip

--- Agent/addHouse.php
+++ Agent/addHouse.php
@@ -25,8 +25,8 @@
                 dataType: "json",
                 data: {"address": $("#zip").val() },
                 success: function(data,status) {
-                    alert(data[]);
-                     $("#city").html(data.results);
+                    alert(data);
+                     $("#city").html(data);
                 },
                 complete: function(data,status) { //optional, used for debugging purposes
                      alert(status);
