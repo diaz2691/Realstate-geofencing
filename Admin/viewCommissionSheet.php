2b3936b35cc71f3a6ebabf8dba57f3a2b475650d
Jose Diaz <jodiaz@csumb.edu>
2017-01-22 23:32:45 -0800

    prototype of commission broker fee calculations - testing needed

--- Admin/viewCommissionSheet.php
+++ Admin/viewCommissionSheet.php
@@ -97,7 +97,7 @@ To change this template use Tools | Templates.
                 echo "<td>" . $result['lastName'] . "</td>";
                 echo "<td>" . htmlspecialchars($result['address'])." ".$result['city'].", ".$result['state']." ".$result['zip']."</td>";
                 echo "<td>" . htmlspecialchars($result['date']) . "</td>";
-                echo "<td>" . htmlspecialchars(date("d-m-Y", $result['settlementDate'])) . "</td>";
+                echo "<td>" . htmlspecialchars(date_format( $result['settlementDate'],"d-m-Y")) . "</td>";
 
              ?>   
 
