cacb30219714ce9fdca3b46f6e82534bee930cc2
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-01-25 18:59:27 -0600

    Retesting the functionality of the search bar on the customer page

--- Admin/viewHouses.php
+++ Admin/viewHouses.php
@@ -18,7 +18,7 @@
 
         
         <!--
-To change this template use Tools | Templates
+To change this template use Tools | Template
 -->
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
