8dcc6ed7eeb123dc57089450691bdc8a22d71394
Jose Diaz <jodiaz@csumb.edu>
2017-02-02 14:12:22 -0800

    testing import php

--- Admin/viewCommissionSheet.php
+++ Admin/viewCommissionSheet.php
@@ -148,7 +148,7 @@ To change this template use Tools | Templates.
            if (this.readyState == 4 )//&& this.status == 200) 
            {
             var response = JSON.parse(xhr.responseText);
-            alert(response['message']);
+            alert(response.message);
            }
          
         }
