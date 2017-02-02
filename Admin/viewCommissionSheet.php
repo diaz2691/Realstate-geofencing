384c9780ed81a580a563eddc379b321d5bcceb7e
Jose Diaz <jodiaz@csumb.edu>
2017-02-02 11:23:13 -0800

    testing importing php file

--- Admin/viewCommissionSheet.php
+++ Admin/viewCommissionSheet.php
@@ -1,6 +1,6 @@
 <?php
     require("../databaseConnection.php");  
-    require("../../key/rKey.php");
+    require("../keys/refreshKeyAdobe.php");
     session_start();
     $dbConn = getConnection();
 
