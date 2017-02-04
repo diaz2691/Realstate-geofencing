4e50ae240e2a9c9e41a3355dcfa7ed5f2d173701
Jose Diaz <jodiaz@csumb.edu>
2017-02-03 16:44:49 -0800

    testing to return pdf from file

--- Admin/viewCommissionSheet.php
+++ Admin/viewCommissionSheet.php
@@ -116,7 +116,8 @@ To change this template use Tools | Templates.
               </td> 
 
               <td>
-                   <input class="option" type="submit" value="Send" name="Send" onClick="sendComm(<?=$result['commId']?>)"/>
+                <form action="commisionSheet.php?hello=123"
+                   <input class="option" type="submit" value="Send" name="Send"/>
               </td>
 
               <td>
