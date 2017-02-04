7914573b954c8e17e849fb3da0a2c8dfa004c389
Jose Diaz <jodiaz@csumb.edu>
2017-02-03 16:39:10 -0800

    testing to return pdf from file

--- Admin/viewCommissionSheet.php
+++ Admin/viewCommissionSheet.php
@@ -144,8 +144,7 @@ To change this template use Tools | Templates.
         {
           if (this.readyState == 4) // && this.status == 401) 
            {
-             var response = pdf.responseText;
-             alert(response);
+             
             
            return ;
            }
