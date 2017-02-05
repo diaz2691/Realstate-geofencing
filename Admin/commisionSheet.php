813ba2da70769fe2f06d6b75a7f5ef58badb5fe4
Jose Diaz <jodiaz@csumb.edu>
2017-02-05 01:17:33 -0800

    testing to get id after uploading file

--- Admin/commisionSheet.php
+++ Admin/commisionSheet.php
@@ -66,7 +66,7 @@ $pdf->Cell(0,10,'Agent Signature                              Date              
 
 if(isset($_POST['token']))
 {
-	$file_contents = $pdf->Output($_GET['commId'] . ".pdf","S");
+	$file_contents = $pdf->Output($_POST['commId'] . ".pdf","S");
 	echo '<script>
 		console.log("didnt work");
 		 var token = '.$_POST['token'].';
