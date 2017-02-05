0af796193a2826949be080bf7a144d80fd767b65
Jose Diaz <jodiaz@csumb.edu>
2017-02-05 01:28:54 -0800

    testing to get id after uploading file

--- Admin/viewCommissionSheet.php
+++ Admin/viewCommissionSheet.php
@@ -199,7 +199,7 @@ To change this template use Tools | Templates.
            if (this.readyState == 4) // && this.status == 401) 
            {
             var response = JSON.parse(xhr.responseText);
-            alert(response.access_token);
+            console.log(response.access_token);
             
            return response.access_token;
            }
