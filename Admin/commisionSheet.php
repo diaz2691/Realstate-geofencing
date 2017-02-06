1171e4f050d0fbf335436af45ba0c669e7cc6168
Jose Diaz <jodiaz@csumb.edu>
2017-02-06 11:22:37 -0800

    testing to get id after uploading file

--- Admin/commisionSheet.php
+++ Admin/commisionSheet.php
@@ -81,9 +81,9 @@ else
 
 <script>
 
-console.log( "<?php  $ken; ?>");
+//console.log( "<?php  $ken; ?>");
 var data = new FormData();
-data.append("File", "<?php $file_contents ?>");
+//data.append("File", "<?php $file_contents ?>");
 data.append("File-Name", "<?php echo $_POST['commId']?>");
 
 var xhr = new XMLHttpRequest();
