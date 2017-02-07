4f634534f5b54a27d336fcd1733815fc343a6d7d
Jose Diaz <jodiaz@csumb.edu>
2017-02-07 12:01:07 -0800

    Adding file to run script

--- PQLogin/runScript.php
+++ PQLogin/runScript.php
@@ -1,13 +1,16 @@
 <?php
 
 
-exec("PQLogin.py" , $out,$ret );
+//exec("PQLogin.py" , $out,$ret );
 //$output = shell_exec("PQLogin.py");
-foreach ($out as $line)
-{
-    echo $line . "<br />";
-}
+// foreach ($out as $line)
+// {
+//     echo $line . "<br />";
+// }
 
 //echo $ret;
+
+$check = passthru('python PQLogin.py ');
+echo $check;
 
 ?>
