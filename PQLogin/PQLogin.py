b235a20497316e8419dadf875265892c316193c0
Jose Diaz <jodiaz@csumb.edu>
2017-02-07 12:26:29 -0800

    Adding file to run script

--- PQLogin/PQLogin.py
+++ PQLogin/PQLogin.py
@@ -15,20 +15,21 @@ driver.get("https://pqweb.parcelquest.com/#login")
 # Fill the login form and submit it
 username = "baycapital"#sys.argv[2]
 password = "realestate"#sys.argv[3]
-driver.find_element_by_id('txtName').send_keys(username)
-driver.find_element_by_id('txtPwd').send_keys(password)
-driver.find_element_by_id('loginPanel').submit() #figure out what to do with this
+
+#driver.find_element_by_id('txtName').send_keys(username)
+#driver.find_element_by_id('txtPwd').send_keys(password)
+#driver.find_element_by_id('loginPanel').submit() #figure out what to do with this
 
 # Now connected to the home page
 # Click on 3 links in order to reach the page I want to scrape
 
 # Select and print an interesting element by its ID
 try:
-    page = driver.find_element_by_class_name('caption') #change the page loginfo to whatever it is on PQ
+   # page = driver.find_element_by_class_name('caption') #change the page loginfo to whatever it is on PQ
     print (page.text)
     auth = False
-    if("You are logged in as" in page.text):
-        auth = True
+    #if("You are logged in as" in page.text):
+     #   auth = True
 except:
     auth = False
     
