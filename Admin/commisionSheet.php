a1e5cb248d99959a86a77ee6a45cd62503aa29bb
Jose Diaz <jodiaz@csumb.edu>
2017-02-06 20:26:56 -0800

    testing to get id after uploading file

--- Admin/commisionSheet.php
+++ Admin/commisionSheet.php
@@ -67,7 +67,7 @@ $pdf->Cell(0,10,'Agent Signature                              Date              
 if(isset($_POST['token']))
 {
 	$ken = $_POST['token'];
-	//$pdf->Output('../keys/commissionSheetTest.pdf','F');
+	$pdf->Output('../keys/commissionSheetTest.pdf','F');
 
 	
 	
@@ -88,7 +88,7 @@ else
 var token = "<?php echo $ken; ?>";
 
  var data = new FormData();
- data.append("File", "../keys/commissionSheetTest.pdf");
+ data.append("File", "../keys/guide.pdf");
  data.append("File-Name", "Commission Sheet");
 
 var xhr = new XMLHttpRequest();
