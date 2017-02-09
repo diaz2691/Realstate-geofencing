06db26a432f5134e0f8e59bcc31b610ee156ef70
Jose Diaz <jodiaz@csumb.edu>
2017-02-09 14:15:33 -0800

    Fixing py script

--- PQLogin/PQLogin.py
+++ PQLogin/PQLogin.py
@@ -36,12 +36,12 @@ driver.get("https://pqweb.parcelquest.com/login")
 # username = "baycapital"#sys.argv[2]
 # password = "realestate"#sys.argv[3]
 time.sleep(5)
- driver.find_element_by_id('txtName').send_keys(username)
- driver.find_element_by_id('txtPwd').send_keys(password)
- driver.find_element_by_xpath('//*[@id="content"]/div/input').click() #figure out what to do with this
+driver.find_element_by_id('txtName').send_keys(username)
+driver.find_element_by_id('txtPwd').send_keys(password)
+driver.find_element_by_xpath('//*[@id="content"]/div/input').click() #figure out what to do with this
 # #page = driver.find_element_by_class_name('message')
 
- time.sleep(3)
+time.sleep(3)
 
 #dropdown = driver.find_element_by_id('QuickSearch_CountyId')
 #options = dropdown.find_elements_by_tag_name("option")
