71a158ed2d4c4ada62b577f4c0b4d96b15a60da5
Jose Diaz <jodiaz@csumb.edu>
2017-02-08 23:19:14 -0800

    Fixing py script

--- PQLogin/PQLogin.py
+++ PQLogin/PQLogin.py
@@ -17,7 +17,7 @@ from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.support.ui import Select # for <SELECT> HTML form
 # import sys
 
-driver = webdriver.Firefox()
+driver = webdriver.PhantomJS()
 
 # driver = webdriver.PhantomJS('/Users/Brayanne/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
 # On Windows, use: webdriver.PhantomJS('C:\phantomjs-1.9.7-windows\phantomjs.exe')
@@ -25,12 +25,12 @@ driver = webdriver.Firefox()
 # Service selection
 # Here I had to select my school among others 
 driver.get("https://pqweb.parcelquest.com/login")
-assert "Login | ParcelQuest" in driver.title
+
 # Login page (https://cas.ensicaen.fr/cas/login?service=https%3A%2F%2Fshibboleth.ensicaen.fr%2Fidp%2FAuthn%2FRemoteUser)
 # Fill the login form and submit it
 username = "baycapital"#sys.argv[2]
 password = "realestate"#sys.argv[3]
-
+print (driver)
 #driver.find_element_by_css_selector('#txtName').send_keys(username)
 #driver.find_element_by_css_selector('#txtPwd').send_keys(password)
 #driver.find_element_by_id('loginPanel').submit() #figure out what to do with this
@@ -39,13 +39,13 @@ password = "realestate"#sys.argv[3]
 # Click on 3 links in order to reach the page I want to scrape
 
 # Select and print an interesting element by its ID
-try:
-   # page = driver.find_element_by_class_name('caption') #change the page loginfo to whatever it is on PQ
-    print (page.text)
-    auth = False
-    #if("You are logged in as" in page.text):
-     #   auth = True
-except:
-    auth = False
+# try:
+#    # page = driver.find_element_by_class_name('caption') #change the page loginfo to whatever it is on PQ
+#     print (page.text)
+#     auth = False
+#     #if("You are logged in as" in page.text):
+#      #   auth = True
+# except:
+#     auth = False
     
-print (auth)    
+# print (auth)    
