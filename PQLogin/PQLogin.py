147783c4304892dc8d34ef905888f1c3a41ca5f7
Jose Diaz <jodiaz@csumb.edu>
2017-02-08 23:23:49 -0800

    Fixing py script

--- PQLogin/PQLogin.py
+++ PQLogin/PQLogin.py
@@ -24,8 +24,8 @@ driver = webdriver.PhantomJS("/usr/bin/phantomjs")
 
 # Service selection
 # Here I had to select my school among others 
-driver.get("https://pqweb.parcelquest.com/login")
-
+#driver.get("https://pqweb.parcelquest.com/login")
+driver.get("www.google.com")
 # Login page (https://cas.ensicaen.fr/cas/login?service=https%3A%2F%2Fshibboleth.ensicaen.fr%2Fidp%2FAuthn%2FRemoteUser)
 # Fill the login form and submit it
 username = "baycapital"#sys.argv[2]
