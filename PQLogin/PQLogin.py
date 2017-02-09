5949ff43e07d7656a0243f96e9032baa3b565636
Jose Diaz <jodiaz@csumb.edu>
2017-02-09 14:19:04 -0800

    Fixing py script

--- PQLogin/PQLogin.py
+++ PQLogin/PQLogin.py
@@ -28,7 +28,7 @@ driver = webdriver.PhantomJS()
 
 # Service selection
 # Here I had to select my school among others 
-driver.get("https://pqweb.parcelquest.com/login")
+driver.get("https://pqweb.parcelquest.com/#login")
 #driver.get("www.python.org")
 #print ("hello")
 # Login page (https://cas.ensicaen.fr/cas/login?service=https%3A%2F%2Fshibboleth.ensicaen.fr%2Fidp%2FAuthn%2FRemoteUser)
