826e3ace6c71730585d22ebd43514d00653d3149
Jose Diaz <jodiaz@csumb.edu>
2017-02-08 23:12:47 -0800

    Fixing py script

--- PQLogin/PQLogin.py
+++ PQLogin/PQLogin.py
@@ -13,6 +13,7 @@
 # 	print (page.content)
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.support.ui import Select # for <SELECT> HTML form
 # import sys
 
@@ -24,7 +25,7 @@ driver = webdriver.PhantomJS()
 # Service selection
 # Here I had to select my school among others 
 driver.get("https://pqweb.parcelquest.com/login")
-
+assert "Login | ParcelQuest" in driver.title
 # Login page (https://cas.ensicaen.fr/cas/login?service=https%3A%2F%2Fshibboleth.ensicaen.fr%2Fidp%2FAuthn%2FRemoteUser)
 # Fill the login form and submit it
 username = "baycapital"#sys.argv[2]
