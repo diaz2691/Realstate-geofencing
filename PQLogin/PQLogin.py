58f03cc20c962d9d4c04059ab0a18fdcbb23d84d
Jose Diaz <jodiaz@csumb.edu>
2017-02-08 23:30:10 -0800

    Fixing py script

--- PQLogin/PQLogin.py
+++ PQLogin/PQLogin.py
@@ -17,7 +17,7 @@ from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.support.ui import Select # for <SELECT> HTML form
 # import sys
 
-driver = webdriver.PhantomJS("/usr/bin/phantomjs")
+driver = webdriver.PhantomJS()
 
 # driver = webdriver.PhantomJS('/Users/Brayanne/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
 # On Windows, use: webdriver.PhantomJS('C:\phantomjs-1.9.7-windows\phantomjs.exe')
