8bcdffe4c17f8b247fb308ad8b4345f17dd7884a
Jose Diaz <jodiaz@csumb.edu>
2017-02-09 11:44:50 -0800

    Fixing py script

--- PQLogin/PQLogin.py
+++ PQLogin/PQLogin.py
@@ -20,7 +20,7 @@ import time
 # from selenium.webdriver.support.ui import Select # for <SELECT> HTML form
 # import sys
 #gc.collect()
-driver = webdriver.PhantomJS(service_log_path=os.path.devnull)
+#driver = webdriver.PhantomJS(service_log_path=os.path.devnull)
 
 # driver = webdriver.PhantomJS('/Users/Brayanne/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
 # On Windows, use: webdriver.PhantomJS('C:\phantomjs-1.9.7-windows\phantomjs.exe')
