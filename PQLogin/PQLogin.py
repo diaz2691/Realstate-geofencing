7cb99ef6efe4b3887dce131ccae8957a34292ab4
Jose Diaz <jodiaz@csumb.edu>
2017-02-09 16:34:54 -0800

    testing script completed

--- PQLogin/PQLogin.py
+++ PQLogin/PQLogin.py
@@ -12,65 +12,65 @@
 # 	page = c.get('https://pqweb.parcelquest.com/#home')
 # 	print (page.content)
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import os
-import gc
-import time
-# from selenium.webdriver.support.ui import Select # for <SELECT> HTML form
-# import sys
-#gc.collect()
-driver = webdriver.PhantomJS('/usr/bin/phantomjs')
-#driver = webdriver.PhantomJS()
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# import os
+# import gc
+# import time
+# # from selenium.webdriver.support.ui import Select # for <SELECT> HTML form
+# # import sys
+# #gc.collect()
+# driver = webdriver.PhantomJS('/usr/bin/phantomjs')
+# #driver = webdriver.PhantomJS()
 
-# driver = webdriver.PhantomJS('/Users/Brayanne/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
-# On Windows, use: webdriver.PhantomJS('C:\phantomjs-1.9.7-windows\phantomjs.exe')
+# # driver = webdriver.PhantomJS('/Users/Brayanne/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
+# # On Windows, use: webdriver.PhantomJS('C:\phantomjs-1.9.7-windows\phantomjs.exe')
 
-# Service selection
-# Here I had to select my school among others 
-driver.get("https://pqweb.parcelquest.com/#login")
-#driver.get("www.python.org")
-#print ("hello")
-# Login page (https://cas.ensicaen.fr/cas/login?service=https%3A%2F%2Fshibboleth.ensicaen.fr%2Fidp%2FAuthn%2FRemoteUser)
-# Fill the login form and submit it
-username = "baycapital"
-password = "realestate"
-time.sleep(5)
-driver.find_element_by_id('txtName').click()
-driver.find_element_by_id('txtName').clear()
-driver.find_element_by_id('txtName').send_keys(username)
-print (driver.find_element_by_id('txtName').get_attribute('value'))
+# # Service selection
+# # Here I had to select my school among others 
+# driver.get("https://pqweb.parcelquest.com/#login")
+# #driver.get("www.python.org")
+# #print ("hello")
+# # Login page (https://cas.ensicaen.fr/cas/login?service=https%3A%2F%2Fshibboleth.ensicaen.fr%2Fidp%2FAuthn%2FRemoteUser)
+# # Fill the login form and submit it
+# username = "baycapital"
+# password = "realestate"
+# time.sleep(5)
+# driver.find_element_by_id('txtName').click()
+# driver.find_element_by_id('txtName').clear()
+# driver.find_element_by_id('txtName').send_keys(username)
+# print (driver.find_element_by_id('txtName').get_attribute('value'))
 
-driver.find_element_by_id('txtPwd').send_keys(password)
-driver.find_element_by_xpath('//*[@id="content"]/div/input').click() #figure out what to do with this
-# #page = driver.find_element_by_class_name('message')
+# driver.find_element_by_id('txtPwd').send_keys(password)
+# driver.find_element_by_xpath('//*[@id="content"]/div/input').click() #figure out what to do with this
+# # #page = driver.find_element_by_class_name('message')
 
-time.sleep(3)
+# time.sleep(3)
 
-dropdown = driver.find_element_by_id('QuickSearch_CountyId')
-options = dropdown.find_elements_by_tag_name("option")
-for option in options:
-	print(option.text)
-    #print(option.get_attribute("value"))
-#print (driver.current_url)
-driver.quit()
+# dropdown = driver.find_element_by_id('QuickSearch_CountyId')
+# options = dropdown.find_elements_by_tag_name("option")
+# for option in options:
+# 	print(option.text)
+#     #print(option.get_attribute("value"))
+# #print (driver.current_url)
+# driver.quit()
 
-#print (page.text)
+# #print (page.text)
 
-# Now connected to the home page
-# Click on 3 links in order to reach the page I want to scrape
+# # Now connected to the home page
+# # Click on 3 links in order to reach the page I want to scrape
 
-# Select and print an interesting element by its ID
-# try:
-#    # page = driver.find_element_by_class_name('caption') #change the page loginfo to whatever it is on PQ
-#     print (page.text)
-#     auth = False
-#     #if("You are logged in as" in page.text):
-#      #   auth = True
-# except:
-#     auth = False
+# # Select and print an interesting element by its ID
+# # try:
+# #    # page = driver.find_element_by_class_name('caption') #change the page loginfo to whatever it is on PQ
+# #     print (page.text)
+# #     auth = False
+# #     #if("You are logged in as" in page.text):
+# #      #   auth = True
+# # except:
+# #     auth = False
     
-# print (auth)   
+# # print (auth)   
 
 
 
