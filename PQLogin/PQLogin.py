65d84fa9913f4eed65b2dd0006ed4a5280350ce1
Jose Diaz <jodiaz@csumb.edu>
2017-02-09 16:34:32 -0800

    testing script completed

--- PQLogin/PQLogin.py
+++ PQLogin/PQLogin.py
@@ -20,7 +20,7 @@ import time
 # from selenium.webdriver.support.ui import Select # for <SELECT> HTML form
 # import sys
 #gc.collect()
-driver = webdriver.PhantomJS()
+driver = webdriver.PhantomJS('/usr/bin/phantomjs')
 #driver = webdriver.PhantomJS()
 
 # driver = webdriver.PhantomJS('/Users/Brayanne/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
@@ -33,21 +33,26 @@ driver.get("https://pqweb.parcelquest.com/#login")
 #print ("hello")
 # Login page (https://cas.ensicaen.fr/cas/login?service=https%3A%2F%2Fshibboleth.ensicaen.fr%2Fidp%2FAuthn%2FRemoteUser)
 # Fill the login form and submit it
-# username = "baycapital"#sys.argv[2]
-# password = "realestate"#sys.argv[3]
+username = "baycapital"
+password = "realestate"
 time.sleep(5)
+driver.find_element_by_id('txtName').click()
+driver.find_element_by_id('txtName').clear()
 driver.find_element_by_id('txtName').send_keys(username)
+print (driver.find_element_by_id('txtName').get_attribute('value'))
+
 driver.find_element_by_id('txtPwd').send_keys(password)
 driver.find_element_by_xpath('//*[@id="content"]/div/input').click() #figure out what to do with this
 # #page = driver.find_element_by_class_name('message')
 
 time.sleep(3)
 
-#dropdown = driver.find_element_by_id('QuickSearch_CountyId')
-#options = dropdown.find_elements_by_tag_name("option")
-#for option in options:
-#    print(option.get_attribute("value"))
-print (driver.current_url)
+dropdown = driver.find_element_by_id('QuickSearch_CountyId')
+options = dropdown.find_elements_by_tag_name("option")
+for option in options:
+	print(option.text)
+    #print(option.get_attribute("value"))
+#print (driver.current_url)
 driver.quit()
 
 #print (page.text)
@@ -65,4 +70,87 @@ driver.quit()
 # except:
 #     auth = False
     
-# print (auth)    
+# print (auth)   
+
+
+
+
+
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
+import gc
+import time
+
+driver = webdriver.PhantomJS('/usr/bin/phantomjs')
+
+driver.get("https://pqweb.parcelquest.com/#login")
+
+username = "baycapital"
+password = "realestate"
+time.sleep(5)
+driver.find_element_by_id('txtName').click()
+driver.find_element_by_id('txtName').clear()
+driver.find_element_by_id('txtName').send_keys(username)
+print (driver.find_element_by_id('txtName').get_attribute('value'))
+
+driver.find_element_by_id('txtPwd').send_keys(password)
+driver.find_element_by_xpath('//*[@id="content"]/div/input').click() #figure out what to do with this
+
+time.sleep(3)
+
+county = "Monterey, CA"
+streetAddress = "1131 carson st"
+
+dropdown = driver.find_element_by_id('QuickSearch_CountyId')
+options = dropdown.find_elements_by_tag_name("option")
+for option in options:
+	if option.text == county:
+		option.click()
+		break
+driver.find_element_by_id('QuickSearch_StreetAddress').send_keys(streetAddress)
+driver.find_element_by_class_name('btnQuickSearch').click()
+time.sleep(6)
+driver.find_element_by_name('viewResults').click()
+time.sleep(3)
+
+listView = driver.find_element_by_id('displaytypeOptions')
+listOption = dropdown.find_elements_by_tag_name("option")
+for opt in listOption:
+	if opt.text == "Detail View":
+		opt.click()
+		break
+time.sleep(2)
+
+totalValue = driver.find_element_by_xpath('//*[@id="assessor-container"]/div[2]/div[4]/table/tbody/tr[1]/td[2]/span')
+squareFeet = driver.find_element_by_xpath('//*[@id="assessor-container"]/div[2]/div[8]/table/tbody/tr[7]/td[2]/span')
+bedrooms = driver.find_element_by_xpath('//*[@id="assessor-container"]/div[2]/div[8]/table/tbody/tr[1]/td[2]/span')
+fullBaths = driver.find_element_by_xpath('//*[@id="assessor-container"]/div[2]/div[8]/table/tbody/tr[2]/td[2]/span')
+apn = driver.find_element_by_xpath('//*[@id="assessor-container"]/div[2]/div[2]/table/tbody/tr[3]/td[2]')
+
+print(totalValue)
+print(squareFeet)
+print(bedrooms)
+print(fullBaths)
+print(apn)
+
+driver.quit() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
