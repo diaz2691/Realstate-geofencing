b67c093dd3e90f3400957a031bbbcb08dedee9f1
Jose Diaz <jodiaz@csumb.edu>
2017-02-10 16:44:03 -0800

    finaly script

--- PQLogin/runScript.php
+++ PQLogin/runScript.php
@@ -17,7 +17,7 @@ session_start();
 $address = "address";
 $county = "county";
 
-$script = passthru('python PQLogin.py 2>&1 ' . $address. ' '.$county);
+$script = shell_exec('python PQLogin.py 2>&1 ' . $address. ' '.$county);
 
 $result = var_dump(json_decode($script, true));
 // foreach ($result as $r ) 
