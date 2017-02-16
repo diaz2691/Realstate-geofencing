d6c42e18c3f9a3b2e7e721d378e5eb45ae75474b
Jose Diaz <jodiaz@csumb.edu>
2017-02-16 09:16:30 -0800

    creating py script to verify license

---
+++ PQLogin/breLic.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
+import gc
+import time
+import json
+import sys
+
+license = '01864213'
+name = ""
+licIssued = ""
+expirationDate = ""
+
+
+# driver = webdriver.PhantomJS('/usr/bin/phantomjs')
+
+driver.get("http://www2.dre.ca.gov/PublicASP/pplinfo.asp")
+time.sleep(1)
+driver.find_element_by_name('LICENSE_ID').send_keys(license)
+
+driver.find_element_by_xpath('//*[@id="main_content"]/div/form/table/tbody/tr[6]/td[2]/input[1]').click()
+
+
+name = driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/font')
+licIssued = driver.find_element_by_xpath('/html/body/table/tbody/tr[8]/td[2]/font')
+expirationDate = driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td[2]/font')
+
+print name
+print licIssued
+print expirationDate
+
+
+
+# D = {"totVal":totalValue[1].get_attribute('innerHTML'), "sqFeet": squareFeet[1].get_attribute('innerHTML'), "bedR": bedrooms[1].get_attribute('innerHTML'), "fullBath": fullBaths[1].get_attribute('innerHTML'), "apn": apn[1].get_attribute('innerHTML')}
+
+# print json.dumps(D)
+
+driver.quit() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
