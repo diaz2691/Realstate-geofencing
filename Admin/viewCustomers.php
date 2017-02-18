4c1d16086d183a2b29ce5e0f9ad358e69fdf739b
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-02-17 19:40:42 -0600

    Adding back search bar

--- Admin/viewCustomers.php
+++ Admin/viewCustomers.php
@@ -92,6 +92,56 @@ To change this template use Tools | Templates.
             .tftable td {font-size:14px;border-width: 1px;padding: 8px;border-style: solid;border-color: #686767;}
             .tftable tr:hover {background-color:#c68c53;}
     </style>
+
+    <!-- CSS styles for standard search box -->
+    <style type="text/css">
+        #tfheader{
+            background-color:#c3dfef;
+        }
+        #tfnewsearch{
+            float:right;
+            padding:20px;
+        }
+        .tftextinput{
+            margin: 0;
+            padding: 5px 15px;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size:14px;
+            border:1px solid #0076a3; border-right:0px;
+            border-top-left-radius: 5px 5px;
+            border-bottom-left-radius: 5px 5px;
+        }
+        .tfbutton {
+            margin: 0;
+            padding: 5px 15px;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size:14px;
+            outline: none;
+            cursor: pointer;
+            text-align: center;
+            text-decoration: none;
+            color: #ffffff;
+            border: solid 1px #0076a3; border-right:0px;
+            background: #0095cd;
+            background: -webkit-gradient(linear, left top, left bottom, from(#00adee), to(#0078a5));
+            background: -moz-linear-gradient(top,  #00adee,  #0078a5);
+            border-top-right-radius: 5px 5px;
+            border-bottom-right-radius: 5px 5px;
+        }
+        .tfbutton:hover {
+            text-decoration: none;
+            background: #007ead;
+            background: -webkit-gradient(linear, left top, left bottom, from(#0095cc), to(#00678e));
+            background: -moz-linear-gradient(top,  #0095cc,  #00678e);
+        }
+        /* Fixes submit button height problem in Firefox */
+        .tfbutton::-moz-focus-inner {
+          border: 0;
+        }
+        .tfclear{
+            clear:both;
+        }
+    </style>
 </head>
     
 
@@ -103,6 +153,14 @@ To change this template use Tools | Templates.
         <br/><br/><h2 id="header2">Customers &#x2713</h2> 
        
         <!--<input type="text" id="search" name="search" placeholder="search customer name...">-->
+
+        <!-- HTML for SEARCH BAR -->
+          <div id="tfheader">
+            <form id="tfnewsearch" method="get" action="viewCustomers.php">
+                    <input type="text" class="tftextinput" name="q" size="21" maxlength="120"><input type="submit" value="search" class="tfbutton">
+            </form>
+          <div class="tfclear"></div>
+          </div>
 
         <table class="tftable" border="1">
        
