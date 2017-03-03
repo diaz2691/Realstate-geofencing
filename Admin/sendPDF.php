625e20ae08cd8e42ad218e17357adc297fdaae05
Jose Diaz <jodiaz@csumb.edu>
2017-03-02 20:52:05 -0800

    adding code to send pdf of house

---
+++ Admin/sendPDF.php
@@ -0,0 +1,21 @@
+<?php
+    header("http://52.11.24.75/keys/1331_Sonoma_Seaside_Ca%20Flyer.pdf");
+ 
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add New Agent Information</title>
+    <meta charset = "utf-8"/>
+    <!-- <script src="//code.jquery.com/jquery-1.11.2.min.js"></script> importing jQuery library-->
+    <link type="text/css" rel="stylesheet" href="editAgentInfo.css">
+   
+
+</head>
+    <body>
+
+        
+    </body>
+</html>
