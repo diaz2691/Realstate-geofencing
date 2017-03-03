44252e8bcfd2ee9d31f4f765b50ad05c29fc4c1f
Jose Diaz <jodiaz@csumb.edu>
2017-03-02 20:53:55 -0800

    adding code to send pdf of house

--- Admin/sendPDF.php
+++ Admin/sendPDF.php
@@ -4,18 +4,3 @@
 ?>
 
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Add New Agent Information</title>
-    <meta charset = "utf-8"/>
-    <!-- <script src="//code.jquery.com/jquery-1.11.2.min.js"></script> importing jQuery library-->
-    <link type="text/css" rel="stylesheet" href="editAgentInfo.css">
-   
-
-</head>
-    <body>
-
-        
-    </body>
-</html>
