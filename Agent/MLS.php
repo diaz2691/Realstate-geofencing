f3f4a06c1f9b7f747789a056fa58109f1fc4ee7c
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-03-04 16:37:27 -0500

    Embedding MLS page into website

---
+++ Agent/MLS.php
@@ -0,0 +1,39 @@
+<?php
+    require("../databaseConnection.php");  
+    session_start();
+    $dbConn = getConnection();
+
+    if(!isset($_SESSION['userId'])) {
+        header("Location: ../index.html?error=wrong username or password");
+    } 
+ ?>
+
+        
+        <!--
+To change this template use Tools | Templates.
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    
+<head>
+    <title></title>
+    
+    <script>
+        
+    </script>
+    
+    <meta charset = "utf-8"/>
+</head>
+    
+
+    <body>
+        <!-- Navigation Bar -->
+        <?php
+            require("agentNav.php");
+        ?>
+
+        <object data=http://www.web-source.net width="600" height="400"> <embed src=http://www.web-source.net width="600" height="400"> </embed> Error: Embedded data could not be displayed. </object>
+        
+        
+    </body>
+</html>
