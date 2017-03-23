9a58ada4c4eb2f7c3d852bb9383afe803b78ef89
Jose Diaz <jodiaz@csumb.edu>
2017-03-22 18:37:08 -0700

    Adding script to send link when client texts

---
+++ Admin/sendFlyer.php
@@ -0,0 +1,33 @@
+<?php
+require('../twilio-php-master/Twilio/autoload.php');
+use Twilio\Twiml;
+
+$response = new Twiml;
+$body = $_REQUEST['Body'];
+
+if( $body == "My home" )
+{
+    $response->message('Hi!');
+}
+else if( $body == 'bye' )
+{
+    $response->message('Entered incorrect phrase');
+}
+//print $response;
+
+
+
+
+// $twilio_phone_number = "+18315851661";
+
+//     $client = new Client($account_sid, $auth_token);
+//     $client->messages->create(
+//     "8312934153",
+//     array(
+//     "From" => $twilio_phone_number,
+//     "Body" => "Flyer",
+//     'mediaUrl' => "http://52.11.24.75/keys/1331_Sonoma_Seaside_CaFlyer.jpg",
+//     )
+//     );
+
+?>
