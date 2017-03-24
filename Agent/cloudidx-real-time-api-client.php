dac172bcaaedaa5361b572490fe402b4f2265a52
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-03-24 16:18:20 -0500

    Implementing the idx API to get house information

--- Agent/cloudidx-real-time-api-client.php
+++ Agent/cloudidx-real-time-api-client.php
@@ -110,7 +110,7 @@ if (isset($responseArray['failure']) or isset($responseArray['message'])) {
 }
 
 foreach ($responseArray['listingSummaryDtoList'] as $homes => $home) {
-    echo "" . $home['address']['streetNumber'] . " " . $home['address']['street'] . " " . $home['address']['city'] . " " . $home['address']['state'] . " " . $home['address']['postalCode'] . " " . $home['listPrice'] . " " . $home['bedrooms'] . " " . $home['fullBaths'] . " " . $home['halfBaths'] . " " $home['status'];
+    echo $home['address']['street'];
 }
 
 
