5f64bd1ebc405820d7e7b4d7307df295e1673739
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-03-24 21:50:48 -0500

    Search bar on agent page

--- Agent/AgentHome.php
+++ Agent/AgentHome.php
@@ -118,9 +118,7 @@ To change this template use Tools | Templates.
 
         $dbConn = getConnection();
             
-        /*if(isset($_GET['searchForm']))
-        {
-
+        if(isset($_GET['searchForm'])){
             if($sortByDate){
                 if($ascending){
                     if($_GET['field'] == "address"){
@@ -217,7 +215,8 @@ To change this template use Tools | Templates.
                     }
                 }
             }
-        else{*/
+        }
+        else{
 
             if($sortByDate){
                 if($ascending){
@@ -246,7 +245,7 @@ To change this template use Tools | Templates.
                         WHERE userId = :userId
                         ORDER BY SUBSTR(LTRIM(address), LOCATE(' ', LTRIM(address))) DESC";
                 }
-            //}
+            }
         }
             $namedParameters = array();
             $namedParameters[':userId'] = $_SESSION['userId'];
