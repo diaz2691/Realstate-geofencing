69c2f4d47ea9047404a2bb39770f285994b054e4
Jose Diaz <jodiaz@csumb.edu>
2017-03-24 19:22:23 -0700

    adding scripts to send text when clicking on button on world view

---
+++ Agent/textAgent.php
@@ -0,0 +1,46 @@
+<?php
+require("../databaseConnection.php");
+
+require('../keys/tKey.php');
+require('../twilio-php-master/Twilio/autoload.php');
+use Twilio\Rest\Client;
+
+
+$dbConn = getConnection();
+$sqls = "SELECT userId, firstName, lastName, phone FROM BuyerInfo WHERE userId =" . $_POST['houseId'];
+$stmts = $dbConn -> prepare($sqls);
+$stmts->execute();       
+$counter = $stmts->fetch();
+
+$sql = "SELECT firstName, lastName FROM UsersInfo WHERE userId =" $counter['userId'];
+$stmt = $dbConn -> prepare($sqls);
+$stmt->execute();       
+$agent = $stmt->fetch();
+
+
+
+$sql = "SELECT firstName, lastName FROM UsersInfo WHERE userId =" $_POST['agentId'];
+$stmt = $dbConn -> prepare($sql);
+$stmt->execute();       
+$curAgent = $stmt->fetch();
+
+
+$twilio_phone_number = "+18315851661";
+
+$client = new Client($account_sid, $auth_token);
+$client->account->messages->create(
+$agent['phone'],
+array(
+"From" => $twilio_phone_number,
+"Body" => "Hey " . $agent['firstName'] . " " . $agent['lastName'] . " This is " . $curAgent['firstName'] . " " . $curAgent['lastName'] . 
+". I have a house that " . $counter['firstName'] . " " . $counter['lastName'] . " might be interested in. 
+Call me at: " . $curAgent['phone'] ,
+));
+
+
+
+
+
+
+
+?>
