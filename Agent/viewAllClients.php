71c0cc367b380d0b7561c6a5aa695db8fb013128
Jose Diaz <jodiaz@csumb.edu>
2017-03-24 20:06:15 -0700

    adding scripts to send text when clicking on button on world view

--- Agent/viewAllClients.php
+++ Agent/viewAllClients.php
@@ -56,7 +56,6 @@ To change this template use Tools | Templates.
               }
 
               xhr.open("POST", "textAgent.php", true);
-              xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
               xhr.setRequestHeader("houseId", houseId);
               xhr.setRequestHeader("agentId", agentId);
               xhr.send();
