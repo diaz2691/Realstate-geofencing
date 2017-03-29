c7924141b2a4af8fb4d2723b2a4bdab9011faf6d
Jose Diaz <jodiaz@csumb.edu>
2017-03-28 19:59:57 -0700

    Fixing text button

--- Agent/textAgent.php
+++ Agent/textAgent.php
@@ -8,7 +8,7 @@ use Twilio\Rest\Client;
 
 $dbConn = getConnection();
 
-$sqls = "SELECT userId, firstName, lastName, phone FROM BuyerInfo WHERE userId =" . $_POST['houseId'];
+$sqls = "SELECT userId, firstName, lastName, phone FROM BuyerInfo WHERE houseId =" . $_POST['houseId'];
 $stmts = $dbConn -> prepare($sqls);
 $stmts->execute();       
 //$counter = $stmts->fetchAll();
