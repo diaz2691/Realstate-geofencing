52fbb74da93bbeb8a75757ab28131de33024ad33
Jose Diaz <jodiaz@csumb.edu>
2017-03-28 20:30:54 -0700

    Fixing text button

--- Agent/textAgent.php
+++ Agent/textAgent.php
@@ -13,7 +13,7 @@ $stmts = $dbConn -> prepare($sqls);
 $stmts->execute();       
 $counter = $stmts->fetch();
 
-$agentOne = "SELECT firstName, lastName FROM UsersInfo WHERE userId =" . $counter['userId'];
+$agentOne = "SELECT firstName, lastName, phone FROM UsersInfo WHERE userId =" . $counter['userId'];
 $stmt = $dbConn -> prepare($agentOne);
 $stmt->execute();       
 $agent = $stmt->fetch();
@@ -29,14 +29,14 @@ $curAgent = $stmt->fetch();
 $twilio_phone_number = "+18315851661";
 
 $client = new Client($account_sid, $auth_token);
-// $client->account->messages->create(
-// $agent['phone'],
-// array(
-// "From" => $twilio_phone_number,
-// "Body" => "Hey " . $agent['firstName'] . " " . $agent['lastName'] . " This is " . $curAgent['firstName'] . " " . $curAgent['lastName'] . 
-// ". I have a house that " . $counter['firstName'] . " " . $counter['lastName'] . " might be interested in. 
-// Call me at: " . $curAgent['phone'] ,
-// ));
+$client->account->messages->create(
+$agent['phone'],
+array(
+"From" => $twilio_phone_number,
+"Body" => "Hey " . $agent['firstName'] . " " . $agent['lastName'] . " This is " . $curAgent['firstName'] . " " . $curAgent['lastName'] . 
+". I have a house that " . $counter['firstName'] . " " . $counter['lastName'] . " might be interested in. 
+Call me at: " . $curAgent['phone'] ,
+));
 
 
 
