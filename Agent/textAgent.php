8ed790643692355b8591d109e6a3cfa3bc11775c
Jose Diaz <jodiaz@csumb.edu>
2017-03-28 20:12:37 -0700

    Fixing text button

--- Agent/textAgent.php
+++ Agent/textAgent.php
@@ -15,7 +15,7 @@ $counter = $stmts->fetchAll();
 
 $agentOne = "SELECT firstName, lastName FROM UsersInfo WHERE userId =" . $counter['userId'];
 $stmt = $dbConn -> prepare($agentOne);
-// $stmt->execute();       
+ $stmt->execute();       
 // $agent = $stmt->fetchAll();
 
 
