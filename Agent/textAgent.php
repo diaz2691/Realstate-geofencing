6654b7d528eafd8f39d52104657e05ba255cebd3
Jose Diaz <jodiaz@csumb.edu>
2017-03-28 19:57:16 -0700

    Fixing text button

--- Agent/textAgent.php
+++ Agent/textAgent.php
@@ -11,32 +11,32 @@ $dbConn = getConnection();
 $sqls = "SELECT userId, firstName, lastName, phone FROM BuyerInfo WHERE userId =" . $_POST['houseId'];
 $stmts = $dbConn -> prepare($sqls);
 $stmts->execute();       
-$counter = $stmts->fetchAll();
+//$counter = $stmts->fetchAll();
 
 $sql = "SELECT firstName, lastName FROM UsersInfo WHERE userId =" . $counter['userId'];
 $stmt = $dbConn -> prepare($sql);
 $stmt->execute();       
-$agent = $stmt->fetchAll();
+//$agent = $stmt->fetchAll();
 
 
 
 $sql = "SELECT firstName, lastName FROM UsersInfo WHERE userId =" . $_POST['agentId'];
 $stmt = $dbConn -> prepare($sql);
 $stmt->execute();       
-$curAgent = $stmt->fetchAll();
+//$curAgent = $stmt->fetchAll();
 
 
 $twilio_phone_number = "+18315851661";
 
-$client = new Client($account_sid, $auth_token);
-$client->account->messages->create(
-$agent['phone'],
-array(
-"From" => $twilio_phone_number,
-"Body" => "Hey " . $agent['firstName'] . " " . $agent['lastName'] . " This is " . $curAgent['firstName'] . " " . $curAgent['lastName'] . 
-". I have a house that " . $counter['firstName'] . " " . $counter['lastName'] . " might be interested in. 
-Call me at: " . $curAgent['phone'] ,
-));
+// $client = new Client($account_sid, $auth_token);
+// $client->account->messages->create(
+// $agent['phone'],
+// array(
+// "From" => $twilio_phone_number,
+// "Body" => "Hey " . $agent['firstName'] . " " . $agent['lastName'] . " This is " . $curAgent['firstName'] . " " . $curAgent['lastName'] . 
+// ". I have a house that " . $counter['firstName'] . " " . $counter['lastName'] . " might be interested in. 
+// Call me at: " . $curAgent['phone'] ,
+// ));
 
 
 
