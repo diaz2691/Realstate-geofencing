a3bf2c4a05911ba8080e10ff79eaeaf20cbc939b
Jose Diaz <jodiaz@csumb.edu>
2017-04-08 21:34:44 -0700

    changing to docusign

--- Admin/viewCommissionSheet.php
+++ Admin/viewCommissionSheet.php
@@ -208,7 +208,7 @@ To change this template use Tools | Templates.
         {
            if (this.readyState == 4) 
            {
-            var response = JSON.parse(xhr.responseText);
+            var response = JSON.stringify(xhr.responseText);
             xhr.abort();
             console.log(response);
 
