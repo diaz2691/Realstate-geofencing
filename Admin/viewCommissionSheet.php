f4fa8d9918641327c1cfa9f4ba79edec1b7220d6
Jose Diaz <jodiaz@csumb.edu>
2017-04-09 00:04:05 -0700

    changing to docusign

--- Admin/viewCommissionSheet.php
+++ Admin/viewCommissionSheet.php
@@ -250,7 +250,8 @@ To change this template use Tools | Templates.
       {
          if (this.readyState == 4) 
          {
-            alert("Sent");
+            var response = JSON.parse(xhr.responseText);
+            alert(response.status);
          }
        
       }
