e246cef68eacf932716a3fecf4fed7847647d16a
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-04-10 10:07:52 -0500

    Trying to fix the error with JD account login

--- Agent/cloudidx-real-time-api-client.php
+++ Agent/cloudidx-real-time-api-client.php
@@ -11,7 +11,7 @@
  */
 
 // Credentials for demo iHomefinder API account
-$clientId = '59157'; //  5-digit iHomefinder demo client ID
+$clientId = 'jorgeedeza'; //  5-digit iHomefinder demo client ID
 $password = '01864213'; // account password
 
 // Provide query parameters
