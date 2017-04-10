2933850a0205f076725fa579b7a2c887f0729b50
Jose Diaz <jodiaz@csumb.edu>
2017-04-10 00:00:08 -0700

    Changing to curl

--- Admin/commisionSheet.php
+++ Admin/commisionSheet.php
@@ -79,7 +79,7 @@ if(isset($_POST['id']))
 	$document = str_replace('\r\n', "", $document);
 	//str_replace("\r\n", "", $document)
 
-	$doc = substr($document,2);
+	$doc = substr($document,0,-1);
 
 	$curl = curl_init();
 
