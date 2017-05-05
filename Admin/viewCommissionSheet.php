2d59adfa69bbaeaedd9610db4dc1c3bf2a8fe586
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-05-05 11:59:20 -0500

    Footer and logo on tab added to all pages

--- Admin/viewCommissionSheet.php
+++ Admin/viewCommissionSheet.php
@@ -135,6 +135,7 @@ To change this template use Tools | Templates.
              ?>         
         </table>
     </body>
+    <?php include('../footer.php'); ?>
 
     <script>
       // function sendComm()
