809329cdf779bca644ec9e691a962c7d86872ba9
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-05-11 18:13:34 -0500

    Adding listings into database

--- Agent/IDXGetFeatured.php
+++ Agent/IDXGetFeatured.php
@@ -1,44 +1,65 @@
 <?php
+	session_start();
 
-$url = 'https://api.idxbroker.com/clients/featured';
+	$url = 'https://api.idxbroker.com/clients/featured';
 
-$method = 'GET';
+	$method = 'GET';
 
-// headers (required and optional)
-$headers = array(
-    'Content-Type: application/x-www-form-urlencoded', // required
-    'accesskey: e1Br0B5DcgaZ3@JXI9qib5', // required - replace with your own
-    'outputtype: json' // optional - overrides the preferences in our API control page
-);
+	// headers (required and optional)
+	$headers = array(
+	    'Content-Type: application/x-www-form-urlencoded', // required
+	    'accesskey: e1Br0B5DcgaZ3@JXI9qib5', // required - replace with your own
+	    'outputtype: json' // optional - overrides the preferences in our API control page
+	);
 
-// set up cURL
-$handle = curl_init();
-curl_setopt($handle, CURLOPT_URL, $url);
-curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+	// set up cURL
+	$handle = curl_init();
+	curl_setopt($handle, CURLOPT_URL, $url);
+	curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
 
-// exec the cURL request and returned information. Store the returned HTTP code in $code for later reference
-$response = curl_exec($handle);
-$code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+	// exec the cURL request and returned information. Store the returned HTTP code in $code for later reference
+	$response = curl_exec($handle);
+	$code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 
-if ($code >= 200 || $code < 300) {
-    $response = json_decode($response,true);
-} else {
-    $error = $code;
-} 
+	if ($code >= 200 || $code < 300) {
+	    $response = json_decode($response,true);
+	} else {
+	    $error = $code;
+	} 
 
-print_r($response);
+	print_r($response);
 
-echo "TEST";
+	echo "TEST";
 
-$keys = array_keys($response);
+	$keys = array_keys($response);
 
 
-for($i = 0; $i < sizeof($keys); $i++){
-	echo $response[$keys[$i]]['address'] . "\n";
-}
+	for($i = 0; $i < sizeof($keys); $i++){
+
+		$sql = "INSERT INTO HouseInfo
+                 (userId, status, address, city, state, zip, bedrooms, bathrooms, price)
+                 VALUES (:userId, :status, :address, :city, :state, :zip, :bedrooms, :bathrooms, :price)";
+          $namedParameters = array();
+          $namedParameters[":userId"] = $_SESSION['userId'];
+          $namedParameters[":status"] = strtolower($response[$keys[$i]]['idxStatus']);
+          $namedParameters[":address"] = $response[$keys[$i]]['address'];
+          $namedParameters[":city"] = $response[$keys[$i]]['cityName'];
+          $namedParameters[":state"] = $response[$keys[$i]]['state'];     
+          $namedParameters[":zip"] = $response[$keys[$i]]['zipcode'];     
+          $namedParameters[":bedrooms"] = $response[$keys[$i]]['bedrooms'];     
+          $namedParameters[":bathrooms"] = $response[$keys[$i]]['totalBaths'];
+          $value = preg_replace('/[\$,]/', '', $response[$keys[$i]]['listingPrice']);
+		  $value = intval($value);     
+          $namedParameters[":price"] = $value;  
+          $stmt = $dbConn -> prepare($sql);
+          $stmt->execute($namedParameters);
+	}
+
+	header("Location: AgentHome.php");
+
 ?>
 
 
