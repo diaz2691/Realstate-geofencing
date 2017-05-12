4a4b95c5cfde8181a7bb7640acc38acbef37e714
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-05-11 20:25:02 -0500

    Avoiding adding house info when it is already in database

--- Agent/IDXGetFeatured.php
+++ Agent/IDXGetFeatured.php
@@ -19,7 +19,7 @@
 
 	function inDatabase($address, $results){
 		foreach($results as $result){
-			echo $result['address'] . ' ' . $address . "<br>";
+			echo $result['address'] . "\t" . $address . "<br>";
 			if($result['address'] === $address){
 				return true;
 			}
